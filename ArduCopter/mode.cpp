2df69c4870eab79c6701f8d72f39a73a97f911de
DESKTOP-HC2GIFV\yano-h <yano.wni@gmail.com>
2018-05-30 01:25:39 +0900

    add new custom flight mode
    - [deines.h] append enum for new flight mode (p.63 / 1)
    - [mode.h] define new Class of new flight mode (p.64 / 2)
    - [mode_new.cpp] create new file (p. 65 / 3)
    - [Copter.h] add new mode instance (p.66 / 4)
    - [mode.cpp] add process for new instance (p.67 / 5)
    - [Parameters.cpp] add explanations of new mode for Mission Planner (p.68 / 6)

--- ArduCopter/mode.cpp
+++ ArduCopter/mode.cpp
@@ -156,6 +156,10 @@ Copter::Mode *Copter::mode_from_mode_num(const uint8_t mode)
             break;
 #endif
 
+        case NEW_MODE:
+            ret = &mode_new;
+            break;
+
         default:
             break;
     }
